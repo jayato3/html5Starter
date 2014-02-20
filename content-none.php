c4245d3d6130e4f1ff0e51b2bb0c13a6588a57ac
jay <jay@o3world.com>
2014-02-20 12:21:30 -0500

    update comments and name spacing

--- content-none.php
+++ content-none.php
@@ -1,30 +1,24 @@
 <?php
 /**
- * The template for displaying a "No posts found" message
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
+ * The default template for displaying content
  */
 ?>
 
-<header class="page-header">
-	<h1 class="page-title"><?php _e( 'Nothing Found', 'twentyfourteen' ); ?></h1>
-</header>
+<h1><?php _e( 'Nothing Found', 'o3world' ); ?></h1>
 
 <div class="page-content">
 	<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-	<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentyfourteen' ), admin_url( 'post-new.php' ) ); ?></p>
+	<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'o3world' ), admin_url( 'post-new.php' ) ); ?></p>
 
 	<?php elseif ( is_search() ) : ?>
 
-	<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentyfourteen' ); ?></p>
+	<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'o3world' ); ?></p>
 	<?php get_search_form(); ?>
 
 	<?php else : ?>
 
-	<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentyfourteen' ); ?></p>
+	<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'o3world' ); ?></p>
 	<?php get_search_form(); ?>
 
 	<?php endif; ?>
